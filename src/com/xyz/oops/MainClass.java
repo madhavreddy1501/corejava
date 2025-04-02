41d1fc576b5b6ddc1255a377beedc85dc6137abb
Pavan Kalyan <pavankalyanmrpk45@gmail.com>
2025-04-02 20:09:34 +0530

    diff between class, abstarct class & interface

---
+++ src/com/xyz/oops/MainClass.java
@@ -0,0 +1,33 @@
+package com.xyz.oops;
+
+class College {
+	//1
+	static void collegeName() {
+		//concrete methods
+	}
+	
+	void coolegeDegeree() {
+		
+	}
+}
+
+abstract class School {
+	//2h
+	void schoolName() {
+		//concrete
+	}
+	
+	//abstract method doesn't have a body
+	abstract void methodName();//abstract method or else non concrete method
+}
+
+interface LowerSchool {
+	//3
+	//interface can allow only abstract method
+	abstract void sample();
+}
+
+
+public class MainClass {
+	
+}
